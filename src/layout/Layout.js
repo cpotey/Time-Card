bd3fab54d11c32c02d7b9d94e410bc9b2ce0b98d
Connor Pote <cpotey@me.com>
2019-09-23 17:34:10 +0100

    Removed React-helmet

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,32 +1,19 @@
 import React, { Component } from 'react'
-import Helmet from 'react-helmet'
-import ThemeContext from '../context/ThemeContext'
+// import Helmet from 'react-helmet'
+// import ThemeContext from '../context/ThemeContext'
 import Navigation from '../components/Navigation'
 import Footer from '../components/Footer'
 
 import '../styles/main.scss'
 
 export default class MainLayout extends Component {
-  static contextType = ThemeContext
 
   render() {
-    const { dark } = this.context
     const { children } = this.props
-
-    let themeClass = ''
-    if (dark) {
-      themeClass = 'dark'
-    } else {
-      themeClass = ''
-    }
 
     return (
       <>
-        <Helmet
-          bodyAttributes={{
-            class: `theme ${themeClass}`,
-          }}
-        ></Helmet>
+        
         <Navigation />
         <main id="main-content">
           <div className="container">{children}</div>
